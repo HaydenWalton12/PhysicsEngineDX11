b98dc007cd3f6ca1a749c014b6710e3a1ee79fc1
HaydenWalton12 <70973137+HaydenWalton12@users.noreply.github.com>
2022-06-14 15:23:42 +0100

    Created It Succesfully.

--- Scene0.h
+++ Scene0.h
@@ -4,6 +4,28 @@
 #include <string>
 
 
+//Struct Used For Interpentration , utilising points of contacts between two bodies 
+struct Contact
+{
+	Vec3 ptOnA_WorldSpace;
+	Vec3 ptOnB_WorldSpace;
+
+	Vec3 ptOnA_LocalSpace;
+	Vec3 ptOnB_LocalSpace;
+	
+
+	//In World Space Coordinates , Normalizzed Direction From Point A-B Either World Or Local Space
+	Vec3 Normal;
+
+	//Value positive when non-pentrating , negative when penetratinhg
+	float _SeperationDistance;
+	float _TimeOfImpact;
+
+	 Body* _BodyA;
+	 Body* _BodyB;
+};
+
+
 class Scene0 : public SceneManager
 {
 public:
@@ -28,8 +50,8 @@ public:
 
 		Surface surface = Surface(Ambient, Diffuse, Specular, SpecularPower);
 
-		_Camera_Position = XMFLOAT3(0.0f, 10.0f, -15.0f);
-		_Camera_Direction = XMFLOAT3(0.0f, -0.5f, 0.5f);
+		_Camera_Position = XMFLOAT3(0.0f, -40.0f, -15.0f);
+		_Camera_Direction = XMFLOAT3(0.0f, -0.05f, 0.05f);
 		_SceneCamera = new Camera(_Camera_Position, _Camera_Direction);
 
 		Body body;
@@ -44,8 +66,8 @@ public:
 		body._Orientation = Quat(0.0f, 0.0f, 0.0f, 1.0f);
 		body._InvMass = 0.0f;
 		body._Position = Vec3(0.0f, -50.0f, 0.0f);
-		body._Shape = new ShapeSphere(30.0f, surface, _pRenderCommand, _Tex,
-		XMFLOAT3(0.0f, -100.0f, 0.0f), XMFLOAT3(10.0f, 10.0f, 10.0f), XMFLOAT3(0.0f, 0.0f, 0.0f));
+		body._Shape = new ShapeSphere(1.0f, surface, _pRenderCommand, _Tex,
+			XMFLOAT3(0.0f, 1.0f, 0.0f), XMFLOAT3(1.0f, 1.0f, 1.0f), XMFLOAT3(0.0f, 0.0f, 0.0f));
 		
 		_SceneBodies.push_back(body);
 
@@ -69,10 +91,11 @@ public:
 			body->AddImpulseLinear(impulse_gravity);
 		}
 
-		for (size_t i = 0; i < _SceneBodies.size(); i++)
+		for (int i = 0; i < _SceneBodies.size(); i++)
 		{
-			for (size_t j = i + 1; j < _SceneBodies.size(); j++)
+			for (int j = i + 1; j < _SceneBodies.size(); j++)
 			{
+			
 				Body* bodyA = &_SceneBodies[i];
 				Body* bodyB = &_SceneBodies[j];
 
@@ -81,27 +104,47 @@ public:
 				{
 					continue;
 				}
-				if (Intersect(bodyA, bodyB))
+				Contact contact;
+				if (Intersect(bodyA, bodyB , contact))
 				{
-					bodyA->_LinearVelocity.Zero();
-					bodyB->_LinearVelocity.Zero();
-
+					ResolveContacts(contact);
 				}
+			
 			}
 		}
 		for (int l = 0; l < _SceneBodies.size(); l++)
 		{
 			Body* body = &_SceneBodies[l];
+			//Keep This Hear - Object Wont move if Contact Is True, Since Velocity is nullified
 			body->_Position += body->_LinearVelocity * 0.01f;
 			body->_Shape->_Object->_ObjectTransformation.SetTranslation(XMFLOAT3(body->_Position.x, body->_Position.y, body->_Position.z));
 			body->_Shape->_Object->_ObjectTransformation.UpdateObject();
 			body->_Shape->_Object->Draw(_SceneCamera);
 
 		}
-
 	}
 
+	void ResolveContacts(Contact contact)
+	{
+		Body* A = contact._BodyA;
+		Body* B = contact._BodyB;
 
+		A->_LinearVelocity.Zero();
+		B->_LinearVelocity.Zero();
+
+		const float tA = A->_InvMass / (A->_InvMass + B->_InvMass);
+		const float tB = B->_InvMass / (A->_InvMass + B->_InvMass);
+
+		const Vec3 ds = contact.ptOnB_WorldSpace - contact.ptOnA_WorldSpace;
+
+		A->_Position += ds * tA;
+		B->_Position -= ds * tB;
+
+
+		A->_Shape->_Object->_ObjectTransformation.SetTranslation(XMFLOAT3(A->_Position.x, A->_Position.y, A->_Position.z));
+		B->_Shape->_Object->_ObjectTransformation.SetTranslation(XMFLOAT3(B->_Position.x, B->_Position.y, B->_Position.z));
+
+	}
 	void DrawUI()
 	{
 		ImGui::Begin("Physics Engine Simulations");
@@ -120,22 +163,38 @@ public:
 		//ImGui::Text("Main Controls:\nNumber 1 - Move To Scene1 \nNumber 2 - Move To Scene2\nNumber 3 - Move To Scene3\nNumber 4 - Move To Scene5\nNumber 5 - Move To Scene5");
 		//ImGui::Text("R - Reset Scene");
 		ImGui::End();
-
-
 	}
 
 
 	void PollInput(float delta_time) override
 	{
+		if (GetAsyncKeyState('S')) {
+			Body* body = &_SceneBodies[0];
+			body->_Position.y = 10.0f;
 
+		}
 	}
 
-	bool Intersect(const Body* A, const Body* B)
+	bool Intersect( Body* A,  Body* B , Contact & contact)
 	{
+
+		contact._BodyA = A;
+		contact._BodyB = B;
+
 		Vec3 ab = B->_Position - A->_Position;
+
+		//Assign Normal Distance Between Objects
+		contact.Normal = ab;
+		contact.Normal.Normalize();
+
 
 		const ShapeSphere* a_sphere = (const ShapeSphere*)A->_Shape;
 		const ShapeSphere* b_sphere = (const ShapeSphere*)B->_Shape;
+
+
+		contact.ptOnA_WorldSpace = A->_Position + contact.Normal * a_sphere->_Radius;
+		contact.ptOnB_WorldSpace = B->_Position - contact.Normal * a_sphere->_Radius;
+
 
 		const float radiusAB = a_sphere->_Radius + b_sphere->_Radius;
 		const float lengthSquare = ab.GetLengthSqr();
@@ -144,8 +203,12 @@ public:
 		{
 			return true;
 		}
+
 		return false;
 	}
+
+	
+
 
 private:
 	Camera* _SceneCamera;
